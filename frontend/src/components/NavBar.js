b44b70895bc0c7234b42c42b34ec855abc22ac5a
Arv2040 <aravcr4085@gmail.com>
2024-02-24 19:06:11 +0530

    added routes to navbar component

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,15 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
     <nav className="fixed top-0 flex w-full px-4 py-2 justify-content-between bg-primary-medium drop-shadow-xl">
       <div className="container mx-auto px-4 flex items-center justify-between">
-        <div className="text-xl text-white">StoryForge.ai</div>
+        <button className="text-xl text-white">
+        <Link to = "/">StoryForge.ai</Link>
+         </button>
         <ul className="flex items-center">
-          <li className="p-4 text-white text-md hover:underline decoration-solid">
-            Home
-          </li>
-          <li className="p-4 text-white text-md hover:underline decoration-solid">
+          <button className="p-4 text-white text-md hover:underline decoration-solid">
+          <Link to = "/ai">Infinite Stories, One Tap Away!</Link>
+          </button>
+          {/* <li className="p-4 text-white text-md hover:underline decoration-solid">
             Product
           </li>
           <li className="p-4 text-white text-md hover:underline decoration-solid">
@@ -17,7 +20,7 @@ export default function NavBar() {
           </li>
           <li className="p-4 text-white text-md hover:underline decoration-solid">
             Contact
-          </li>
+          </li> */}
         </ul>
       </div>
     </nav>
